1ce9e8c0d84fa9888802a808d301f54ad1db2002
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-12 17:56:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one',function(req,res){
     res.send('Article one is requested & will be served!');
 });
 
+app.get('/article-two',function(req,res){
+    res.send('Article two is requested & will be served!');
+});
+
+app.get('/article-three',function(req,res){
+    res.send('Article three is requested & will be served!');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
