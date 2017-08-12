0c2203fdecefd945759ccca957a18902dfd31ee7
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-12 22:37:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,14 +30,14 @@ var articleOne={
 
 function createTemplate(data){
     
-     title=data.title;
-     heading=data.heading;
-     content=data.content;
+     var title=data.title;
+     var heading=data.heading;
+     var content=data.content;
     
     var htmlTemplate=`
             <html>
     <head>
-        <title> $title </title>
+        <title> ${title} </title>
         <meta name="viewport" content="width-device-width,initial-scale=1" />
         <link rel="stylesheet" type="text/css" href="/ui/style.css" />
     </head>
@@ -48,11 +48,11 @@ function createTemplate(data){
         </div>
         
         <div>
-            <h1>$heading</h1>
+            <h1>${heading}</h1>
         </div>
 
         <div>
-            $content;
+            ${content}
         </div>
       </div>
     </body>
