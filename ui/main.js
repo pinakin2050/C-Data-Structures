bb615e350913b573268eb23d7a0151c5b5f23852
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-17 18:27:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.InnerHTML="NEW VALUE";
+var element = document.getElementById('main-text');
+element.innerHTML="NEW VALUE";
