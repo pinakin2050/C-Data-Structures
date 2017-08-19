4ee0e4487431a67f94c2eae1c5141bd1c3cc5d72
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-19 19:19:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,7 +60,7 @@ submit.onclick = function()
                       var list ='' ;
                       for(var i=0;i<names.length;i++)
                         {
-                            list = '<li>'+names[i]+'</li>';
+                            list += '<li>'+names[i]+'</li>';
                         }
                       var ui = document.getElementById("namelist");
                       ui.innerHTML = list;
