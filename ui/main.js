a6b37461048a921a702573e5b2b0d23b0be40f6d
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-19 15:21:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,23 +10,32 @@ img.onclick = function(){
    var interval = setInterval(moveRight,5);   
 };*/
 
-var button = getElementById("counter");
-counter.onclick = function(){
+//counter code
+var button = document.getElementById("counter");
+button.onclick = function()
+   {
             //create req object
-            var request = XMLHttpRequest();
+            var request = new XMLHttpRequest();
             
             //caputer res & store it in a variable
-          request.onreadystatechange = function(){
-              //take some action
-              if(request.status===200){
-                  var counter = request.resposeText;
-                  var span = document.getElementById("count");
-                  span.innerHTML = counter.toString();
-             }
-        }
-    //make http req
-    request.open('GET','http://pinakin2050.imad.hasura-app.io/',true);
-    request.send(null);
-};
+          request.onreadystatechange = function()
+          {
+              if(request.readyState===XMLHttpRequest.DONE)
+                {
+                  //take some action
+                  if(request.status===200)
+                   {
+                      var counter = request.resposeText;
+                      var span = document.getElementById("count");
+                      span.innerHTML = counter.toString();
+                   }
+                }
+                //not done yet
+          };
+          
+        //make http req
+        request.open('GET','http://pinakin2050.imad.hasura-app.io/counter',true);
+        request.send(null);
+    };
 
 
