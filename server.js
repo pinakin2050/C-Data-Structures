82ae1a4c6324f21e32c7a9d15ed2a7c3b0341c99
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-30 20:14:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/db-test',function(req,res)
 
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')];
 }
 app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input,'this-is-random-string');
