a28e98fab486d0d8dd91dfcd1879a9335bb58e78
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-08-31 21:33:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto =require('crypto');
+var bodyParser = require('body-Parser');
 //var env = require('env');
 var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.JSON());
 
 //const env = require('evn2')('./ui/.env');
 //console.log(process.env.DB_PASSWORD);
@@ -59,6 +61,20 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input,'this-is-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbstring = hash(password,salt);
+    pool.query('INSERT INTO  "user"(username,password) VALUES ($1,$2)',[username,dbstring],function(err,result){
+        if(err){
+            res.status(500).send(err.toString);
+        }else{
+            res.send('User successfully created:'+username);
+        }
+    });
 });
 
 var articles={
