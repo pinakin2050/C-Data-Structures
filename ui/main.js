f0da343c44aa830c9354838c4d84349bee67df4a
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-09-02 20:50:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -104,7 +104,7 @@ submit.onclick = function()
           var password= document.getElementById('password').value;
           request.open('POST','http://pinakin2050.imad.hasura-app.io/login',true);
           request.setRequestHeader('Content-Type', 'application/json');
-          request.send(JSON.stringify({username: username,password: password}));
+          request.send(JSON.stringify({"username":"username","password": "password"}));
     };
 
 
