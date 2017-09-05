68125f8c85c47e74678337d45694279e26b15305
PINAKIN BRAHMIN <30834045+pinakin2050@users.noreply.github.com>
2017-09-05 11:32:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,8 +83,9 @@ app.post('/login',function(req,res){
     pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
     
         if(err){
-            res.send("error!!");
+            res.status(500).send(err.toString());
         }else{
+            
             if(result.rows.length === 0){
                 res.status(403).send('USERNAME/PASSWORD IS INCORRECT!!');
             }else{ //match the password 
